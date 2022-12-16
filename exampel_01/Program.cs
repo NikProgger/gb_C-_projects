697885c26e49a21c0668085f97adcfd4ff390bc3
Nikita <vinidiktovn@gmail.com>
2022-12-16 19:27:33 +0300

    Выполнение задания 1

---
+++ exampel_01/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Здравствуйте, Ведите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите Второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(b);
+
+ if (a > b)
+    {
+         Console.WriteLine("Первое число " + a + " больше чем второе " + b);
+    }
+            else
+                {
+                    Console.WriteLine("Второе число " + b + " больше чем первое " + a);
+                }
