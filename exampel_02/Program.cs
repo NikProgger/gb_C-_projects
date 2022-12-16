d2048ba7a7c1e0b7878cfe0a4902042ad21c582d
Nikita <vinidiktovn@gmail.com>
2022-12-16 19:51:34 +0300

    Выполнение задания 2

---
+++ exampel_02/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Здравствуйте, ведите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите третье число:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if( b > max)
+{
+    max =  b;
+}
+     if ( c > max)
+    {
+        max = c;
+    }
+
+Console.WriteLine("Максимальное число:" + max);
+
