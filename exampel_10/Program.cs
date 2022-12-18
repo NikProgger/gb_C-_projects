9c1c658da816f5818952a8803e310277d19fc0e9
Nikita <vinidiktovn@gmail.com>
2022-12-18 12:49:37 +0300

    Выполнение Вэбинара 2

---
+++ exampel_10/Program.cs
@@ -0,0 +1,16 @@
+﻿int main(int a)
+{
+    int result = ((a / 10) % 10);
+    return result;
+}
+
+    Console.Write("Введите 3х-значное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int amount = number.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+    Console.WriteLine($"Второе число:{main(number)}");
---
+++ exampel_13/Program.cs
@@ -0,0 +1,21 @@
+﻿int main(int number)
+        {
+            int result = -1;
+            if (number >= 100)
+            {
+                while (number > 999)
+                {
+                    number = number / 10;
+                }
+                result = number % 10;
+            }
+            return result; 
+        }
+        
+        Console.Write("Введите 3х-значное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (main(number1) == -1)
+Console.WriteLine("Третьей цифры нет");
+else
+Console.WriteLine($"Третье число это {main(number1)}");
