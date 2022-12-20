241f77e3c9f62e4ba023fc60f1b6fecaea27c7af
Nikita <vinidiktovn@gmail.com>
2022-12-20 20:35:45 +0300

    work 19.21.23

---
+++ exampel_19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите пятизначное  число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine(number + " -> Да ");
+    }
+    else
+    {
+        Console.WriteLine(number + " -> Нет ");
+    }
+}
+else
+{
+    Console.WriteLine("ОШИБКА:" + number +" Не является пятизначным числом ");
+}
---
+++ example_23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число N: "); 
+    int num = Convert.ToInt32(Console.ReadLine());
+    int i = 0;
+
+    while (i <= num)
+    {
+        i++;
+    int result = (i*i*i);
+    Console.Write(result);
+    }
