81d5a6d63e463c8997f57cbdf0304f526137439d
Nikita <vinidiktovn@gmail.com>
2022-12-21 14:10:33 +0300

    Выполнение урока 4

---
+++ exampel_25/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
+int i = 1;
+
+while (i < b )
+{
+    i++;
+step = step * a;
+}
+Console.WriteLine("A в степени B равно: " + step);
---
+++ exampel_29/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите массив чисел: ");
+int i = 1;
+int[] a = new int[8];
+
+a[i] = int.Parse(Console.ReadLine());
+
+    for (i = 0; i < 10; i++)
+{
+Console.WriteLine(a[i]);
+}
+//Подскажите что не так?*
