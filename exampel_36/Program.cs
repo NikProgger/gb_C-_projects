d502dc0e3f0ace19fdf10ed1aef388a70f7ac417
Nikita <vinidiktovn@gmail.com>
2022-12-28 21:46:26 +0300

    Lesson 5

---
+++ exampel_36/Program.cs
@@ -0,0 +1,40 @@
+﻿int size = ReadInt("Введите размерность массива: ");
+int min = ReadInt("Введите минимальное число массива: ");
+int max = ReadInt("Введите максимальное число массива: ");
+int [] num = new int[size];
+
+FillArrayRandomNumbers(num);
+PrintArray(num);
+int res = 0;
+
+for (int i = 0; i < num.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        res += num[i];
+    }
+}
+Console.WriteLine("Сумма элементов нечетных позиций " + res);
+
+void FillArrayRandomNumbers(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ exampel_38/Program.cs
@@ -0,0 +1,43 @@
+﻿int size = 10;
+int[] num = new int[size];
+FillArrayRandomNumbers(num);
+PrintArray(num);
+
+int max = num[0];
+int min = num[0];
+
+for (int i = 0; i < num.Length; i++)
+{
+    if (num[i] > max)
+    {
+        max = num[i];
+    }
+    else if (num[i] < min)
+    {
+        min = num[i];
+    }
+}
+
+Console.WriteLine("Минимальное число: " + min);
+Console.WriteLine("Минимальное число: " + max);
+Console.WriteLine($"Разница между максимальным и минимальным числами: {max -min}");
+
+
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 555);
+    }
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
